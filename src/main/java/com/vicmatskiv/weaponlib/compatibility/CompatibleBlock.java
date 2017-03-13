db17cbf5279b0971a4aa14b950fe65b04e71b326
Victor Matskiv <victor.matskiv@yahoo.com>
2017-03-12 18:42:31 -0700

    Compatible block

---
+++ src/main/java/com/vicmatskiv/weaponlib/compatibility/CompatibleBlock.java
@@ -0,0 +1,19 @@
+package com.vicmatskiv.weaponlib.compatibility;
+
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+
+public class CompatibleBlock {
+
+    public static final CompatibleBlock PLANK = new CompatibleBlock(Blocks.planks);
+    
+    private Block block;
+    
+    private CompatibleBlock(Block block) {
+        this.block = block;
+    }
+
+    public Block getBlock() {
+        return block;
+    }
+}
