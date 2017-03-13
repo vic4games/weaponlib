fd91941a8328e824c854231333476f82e3417f2e
Victor Matskiv <victor.matskiv@yahoo.com>
2017-03-12 18:56:48 -0700

    Introduced compatible item

--- src/main/java/com/vicmatskiv/weaponlib/compatibility/CompatibleItem.java
+++ src/main/java/com/vicmatskiv/weaponlib/compatibility/CompatibleItem.java
@@ -2,11 +2,25 @@ package com.vicmatskiv.weaponlib.compatibility;
 
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
 public abstract class CompatibleItem extends Item {
+        
+    public static final CompatibleItem GUNPOWDER = new CompatibleItem(Items.gunpowder) {};
+    
+    private Item item;
+    public CompatibleItem() {}
+    
+    private CompatibleItem(Item item) {
+        this.item = item;
+    }
+    
+    public Item getItem() {
+        return item;
+    }
 	
 	@Override
 	public final ItemStack onItemRightClick(ItemStack itemStack, World world, EntityPlayer player) {
