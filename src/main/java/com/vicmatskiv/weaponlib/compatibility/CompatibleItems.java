df9a9240e04f0566557a68ae84206c89d0a2ec99
Victor Matskiv <victor.matskiv@yahoo.com>
2017-05-31 15:17:04 -0700

    Merge

--- src/main/java/com/vicmatskiv/weaponlib/compatibility/CompatibleItems.java
+++ src/main/java/com/vicmatskiv/weaponlib/compatibility/CompatibleItems.java
@@ -6,7 +6,6 @@ import net.minecraft.item.Item;
 public class CompatibleItems {
 
     public static final CompatibleItems GUNPOWDER = new CompatibleItems(Items.GUNPOWDER) {};
-
     public static final CompatibleItems DYE = new CompatibleItems(Items.DYE) {};
     public static final CompatibleItems IRON_INGOT = new CompatibleItems(Items.IRON_INGOT) {};
     public static final CompatibleItems EMERALD = new CompatibleItems(Items.EMERALD) {};
@@ -15,6 +14,8 @@ public class CompatibleItems {
     public static final CompatibleItems STRING = new CompatibleItems(Items.STRING) {};
     public static final CompatibleItems COAL = new CompatibleItems(Items.COAL) {};
     public static final CompatibleItems WATER_BUCKET = new CompatibleItems(Items.WATER_BUCKET) {};
+    public static final CompatibleItems STICK = new CompatibleItems(Items.STICK) {};
+    public static final CompatibleItems FLINT_AND_STEEL = new CompatibleItems(Items.FLINT_AND_STEEL) {};
 
     private Item item;
 
